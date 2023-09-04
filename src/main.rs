e63c084dce0b52c4d4476245f36fab930b405eae
Bernardo Meurer <bernardo@meurer.org>
2023-09-04 17:59:57 -0300

    refactor: use humantime

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,7 @@ struct Args {
 }
 
 fn elapsed_subsec(state: &ProgressState, writer: &mut dyn std::fmt::Write) {
-    let seconds = state.elapsed().as_secs();
-    let sub_seconds = (state.elapsed().as_millis() % 1000) / 100;
-    let _ = writer.write_str(&format!("{}.{}s", seconds, sub_seconds));
+    let _ = writer.write_str(&humantime::format_duration(state.elapsed()).to_string());
 }
 
 #[tokio::main(flavor = "multi_thread")]
