f905e5a84385c8752fed4182dd3cd433b92e421f
Bernardo Meurer Costa <bernardo@meurer.org>
2024-06-28 15:18:28 +0000

    chore: update svt-av1

--- nix/overlays/svt-av1-latest.nix
+++ nix/overlays/svt-av1-latest.nix
@@ -1,11 +1,11 @@
 final: prev: {
   svt-av1 = prev.svt-av1.overrideAttrs (_: rec {
-    version = "2.1.0";
+    version = "2.1.1";
     src = final.fetchFromGitLab {
       owner = "AOMediaCodec";
       repo = "SVT-AV1";
       rev = "v${version}";
-      hash = "sha256-yfKnkO8GPmMpTWTVYDliERouSFgQPe3CfJmVussxfHY=";
+      hash = "sha256-3rRIvSO4yIHzeRIQXGT460CMkIoHtlLJKSK/MhM3AHM=";
     };
   });
 }
